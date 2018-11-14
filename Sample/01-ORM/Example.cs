c986871842b7c8ed6f90cc7a7d32fd955656734c
Thibaud Desodt <tibo.desodt@gmail.com>
2018-11-14 11:09:43 +0100

    Add code samples

---
+++ Sample/01-ORM/Example.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Sample.ORM.DataAccess;
+
+namespace Sample._01_ORM
+{
+    class Example
+    {
+        public static async void UseCase(string customerId)
+        {
+            using (var db = new NorthwindContext())
+            {
+                var customer = await db.Customers
+                    .Where(e => e.CustomerId == customerId)
+                    .Include(e => e.Orders)
+                    .SingleOrDefaultAsync();                
+
+                if (customer.Orders.Count > 10)
+                {
+                    customer.ContactTitle = "VIP";
+                }
+
+                await db.SaveChangesAsync();
+            }
+        }
+    }
+}
